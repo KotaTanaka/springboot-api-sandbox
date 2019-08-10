7176d41d3c24bda51d5ac307a3f69999124f2832
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-10 20:48:36 +0900

    feat: 商品Entity/Repository作成

---
+++ src/main/kotlin/com/kotatanaka/goodsapi/domain/entity/GoodsEntity.kt
@@ -0,0 +1,49 @@
+package com.kotatanaka.goodsapi.domain.entity
+
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.sql.Timestamp
+import java.util.Date
+import javax.persistence.Basic
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+/**
+ * 商品テーブルのエンティティ
+ *
+ * @author kotatanaka
+ */
+@Entity
+@Table(name = "goods", schema = "goods_db")
+data class GoodsEntity(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  var id: Int = 0,
+
+  @Basic
+  @Column(name = "name", nullable = false, length = 50)
+  var name: String = "",
+
+  @Basic
+  @Column(name = "description", nullable = true, length = 500)
+  var description: String? = null,
+
+  @Basic
+  @Column(name = "price", nullable = false)
+  var price: Int = 0,
+
+  @Basic
+  @Column(name = "created_at", nullable = false)
+  @CreationTimestamp
+  var createdAt: Timestamp = Timestamp(Date().time),
+
+  @Basic
+  @Column(name = "updated_at", nullable = false)
+  @UpdateTimestamp
+  var updatedAt: Timestamp = Timestamp(Date().time)
+)
---
+++ src/main/kotlin/com/kotatanaka/goodsapi/domain/repository/GoodsRepository.kt
@@ -0,0 +1,14 @@
+package com.kotatanaka.goodsapi.domain.repository
+
+import com.kotatanaka.goodsapi.domain.entity.GoodsEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * [GoodsEntity] のリポジトリ
+ *
+ * @author kotatanaka
+ */
+@Repository
+interface GoodsRepository : JpaRepository<GoodsEntity, Int> {
+}
