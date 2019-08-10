a1b1c4d26b4347bb185c31ee024cb28e5ee0fac5
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-11 02:38:08 +0900

    feat: DateTimeUtil作成

---
+++ src/main/kotlin/com/kotatanaka/goodsapi/util/DateTimeUtil.kt
@@ -0,0 +1,25 @@
+package com.kotatanaka.goodsapi.util
+
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+/**
+ * 日付のユーティリティ
+ *
+ * @author kotatanaka
+ */
+class DateTimeUtil {
+
+  companion object {
+    /**
+     * 年月日時分秒形式にフォーマットする
+     *
+     * @param value Timestamp
+     * @return yyyy/MM/dd HH:mm:ss
+     */
+    fun toSimpleDateStr(value: Timestamp): String {
+      return SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault()).format(value)
+    }
+  }
+}
