e3cc3d7ae6f5f78a22a75fcfa0e95c91a13131e5
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-10 17:03:29 +0900

    first commit

---
+++ src/main/kotlin/com/kotatanaka/goodsapi/app/controller/HelloWorldController.kt
@@ -0,0 +1,17 @@
+package com.kotatanaka.goodsapi.app.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Hello World コントローラー
+ *
+ * @author kotatanaka
+ */
+@RestController
+class HelloWorldController {
+  @GetMapping("/hello")
+  fun hello(): String {
+    return "Hello, World!"
+  }
+}
