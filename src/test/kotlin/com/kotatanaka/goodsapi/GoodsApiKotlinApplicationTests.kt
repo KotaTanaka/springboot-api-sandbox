bbbce47979527ab05f68de85ed35562daec04d8b
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-10 18:37:11 +0900

    refactor: インデント修正

--- src/test/kotlin/com/kotatanaka/goodsapi/GoodsApiKotlinApplicationTests.kt
+++ src/test/kotlin/com/kotatanaka/goodsapi/GoodsApiKotlinApplicationTests.kt
@@ -8,9 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class GoodsApiKotlinApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+  @Test
+  fun contextLoads() {}
 }
