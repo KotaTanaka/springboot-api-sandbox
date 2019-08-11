6dd57ca22476155d9d217ee9c2ebcae3f8ddbcf7
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-11 20:39:25 +0900

    feat: RestControllerAdviceでリクエストパラメータをログ出力

---
+++ src/main/kotlin/com/kotatanaka/goodsapi/app/handler/RequestHandler.kt
@@ -0,0 +1,32 @@
+package com.kotatanaka.goodsapi.app.handler
+
+import com.kotatanaka.goodsapi.domain.enums.GoodsParams
+import com.kotatanaka.goodsapi.domain.factory.ObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.WebDataBinder
+import org.springframework.web.bind.annotation.InitBinder
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * リクエストパラメータをログ出力するためのハンドラー
+ *
+ * @author kotatanaka
+ */
+@RestControllerAdvice
+class RequestHandler(private val objectMapper: ObjectMapper) {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(RequestHandler::class.java)
+  }
+
+  @InitBinder
+  fun getRequestParam(binder: WebDataBinder, request: HttpServletRequest) {
+    val requestParamMap = if (binder.target != null) {
+      objectMapper.toMap(binder.target as Any)
+    } else null
+
+    val requestId = request.getAttribute(GoodsParams.REQUEST_ID.key)
+    log.info("[$requestId] Request Parameters: $requestParamMap")
+  }
+}
