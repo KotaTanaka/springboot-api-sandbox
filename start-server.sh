d59b51415e6d1cd5f4026568a0e8e952831b3f3a
KotaTanaka <te11k.tanaka@gmail.com>
2020-07-03 15:29:17 +0900

    config: start,stop-dockerシェル削除

--- start-server.sh
+++ start-server.sh
@@ -2,4 +2,22 @@
 # ------------------------------
 # サーバーを起動するスクリプト
 # ------------------------------
-docker-compose exec app /var/app/gradlew bootRun
+DBPATH=./docker/db/mysql_data/goods_manager_db
+RUN="docker-compose exec app /var/app/gradlew bootRun"
+
+echo "=========================="
+echo " Goods Manager API Server"
+echo "=========================="
+
+# 初回の場合DBが作られるのを待つ
+if [ -d $DBPATH ]; then
+  eval $RUN
+  exit 0
+else
+  /bin/echo -n "Initialize database ... "
+fi
+while [ ! -d $DBPATH ]; do
+  sleep 1
+done
+echo "done."
+eval $RUN
